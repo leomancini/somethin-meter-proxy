a00e8482100fbdd3c0ef511ad6e4b5b315c6071e
Leo Mancini <mancini@leo.gd>
2025-08-27 17:58:28 -0400

    Updating sample data

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3113;
 app.get("/", (req, res) => {
   res.send({
     title: "What price will Ethereum hit in August? - ↑ 5000",
-    probability: 0.19,
+    probability: 0.79,
     volume: 14000000
   });
 });
