abd52c16943f34eeed4a4e5a9d968cdef90c1634
Leo Mancini <mancini@leo.gd>
2025-08-27 17:53:48 -0400

    Adding sample data

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3113;
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+app.get("/", (req, res) => {
+  res.send({
+    title: "What price will Ethereum hit in August? - ↑ 5000",
+    probability: 0.19,
+    volume: 14000000
+  });
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
